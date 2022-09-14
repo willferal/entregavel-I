d7776f453fc1cc1b3902cbe80aa99f9ffd79b1e8
will <100856485+willferal@users.noreply.github.com>
2022-09-14 14:21:11 -0300

    5- added

---
+++ 5-ordenacaoJS.js
@@ -0,0 +1,21 @@
+
+function quicksort(sequencia) {
+    
+    if (sequencia.length <= 1) {
+      return sequencia;
+    }
+  
+    var pivot = sequencia[0];
+    var esquerda = []; 
+    var direita = [];
+  
+    for (var i = 1; i < sequencia.length; i++) {
+        sequencia[i] < pivot ? esquerda.push(sequencia[i]) : direita.push(sequencia[i]);
+    }
+  
+    return quicksort(esquerda).concat(pivot, quicksort(direita));
+  };
+  
+  var unsorted = [23, 45, 16, 37, 3, 99, 22];
+  var sorted = quicksort(unsorted);
+  console.log(sorted)
